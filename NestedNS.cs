4a04eac268611595bf7415c5f07f08d534a62bcf
izzybps <izzybps@gmail.com>
2024-10-22 15:36:13 -0300

    changing namespace to simple

--- NestedNS.cs
+++ NestedNS.cs
@@ -2,40 +2,37 @@
 using System;
 namespace ParentNamespace
 {
-    namespace NestedNamespace
+    class Program
     {
-        class Program
+        public Program(string[] args)
         {
-            public Program(string[] args)
-            {
-                _logger = LogFactory.MakeServiceLog(typeof(BimboB2BServices));
-                _logger.Info("Service B2B Started");
-                InitializeComponent();
-            }
-            private const string PARAM_SCHEDULED_TIME = "ScheduledTime";
-            private const string PARAM_SCHEDULED_TIME_RESTOCK = "ScheduledTimeRestock";
-
-            static void Main(string name, string age, string[] args)
-            {
-                //Your program starts here...
-                Console.WriteLine($"Hello world! My name is {name} and I am {age} years old.");
-            }
+            _logger = LogFactory.MakeServiceLog(typeof(BimboB2BServices));
+            _logger.Info("Service B2B Started");
+            InitializeComponent();
         }
-        class TestClass
+        private const string PARAM_SCHEDULED_TIME = "ScheduledTime";
+        private const string PARAM_SCHEDULED_TIME_RESTOCK = "ScheduledTimeRestock";
+
+        static void Main(string name, string age, string[] args)
         {
-            public TestClass()
-            {
-                _logger = LogFactory.MakeServiceLog(typeof(BimboB2BServices));
-                _logger.Info("Service B2B Started");
-                InitializeComponent();
-            }
-            private const string PARAM_INTERVAL_MINUTES = "IntervalMinutes";
-            private const string PARAM_INTERVAL_MINUTES_RESTOCK = "IntervalMinutesRestock";
-            static void Main(string name, string age, string[] args)
-            {
-                //Your program starts here...
-                Console.WriteLine($"Hello world! My name is {name} and I am {age} years old.");
-            }
+            //Your program starts here...
+            Console.WriteLine($"Hello world! My name is {name} and I am {age} years old.");
+        }
+    }
+    class TestClass
+    {
+        public TestClass()
+        {
+            _logger = LogFactory.MakeServiceLog(typeof(BimboB2BServices));
+            _logger.Info("Service B2B Started");
+            InitializeComponent();
+        }
+        private const string PARAM_INTERVAL_MINUTES = "IntervalMinutes";
+        private const string PARAM_INTERVAL_MINUTES_RESTOCK = "IntervalMinutesRestock";
+        static void Main(string name, string age, string[] args)
+        {
+            //Your program starts here...
+            Console.WriteLine($"Hello world! My name is {name} and I am {age} years old.");
         }
     }
 }
